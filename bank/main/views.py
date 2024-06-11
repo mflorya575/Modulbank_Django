a17f359ba853b376cae23cf97b4daee4e4d500b7
Michael Florya <mih2303@mail.ru>
2024-06-11 20:17:34 +0300

    config template in base_dir & config controller, template & url

--- bank/main/views.py
+++ bank/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    context = {
+        'title': 'Главная',
+    }
+
+    return render(request, 'bank/index.html', context)
