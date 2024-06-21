a8f71b88feec7a503fc4abd0ba32b6d21d22ca62
Michael Florya <mih2303@mail.ru>
2024-06-21 23:46:54 +0300

    add processor & config categories in header

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Новый контекстный процессор
+                'main.context_processors.categories_processor',
             ],
         },
     },
