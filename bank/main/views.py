37759686646d0a239436e5463f8e9695e82fe0be
Michael Florya <mih2303@mail.ru>
2024-06-24 16:55:29 +0300

    update data & styles

--- bank/main/views.py
+++ bank/main/views.py
@@ -66,10 +66,12 @@ def blog_detail(request, slug):
 
 
 def category_detail(request, slug):
+    vacancies = Vacancy.objects.all()
     category = get_object_or_404(Category, slug=slug)
     blogs = Blog.objects.filter(category=category)
     context = {
         'title': category.title,
+        'vacancies': vacancies,
         'category': category,
         'blogs': blogs,
     }
